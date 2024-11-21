d53af68c89c0a99264e944712b01d31402c8b001
febo <febo@anza.xyz>
2024-11-21 00:16:25 +0000

    Inline tweaks

--- p-token/src/processor/initialize_mint2.rs
+++ p-token/src/processor/initialize_mint2.rs
@@ -2,6 +2,7 @@ use pinocchio::{account_info::AccountInfo, ProgramResult};
 
 use super::initialize_mint::{process_initialize_mint, InitializeMint};
 
+#[inline(always)]
 pub fn process_initialize_mint2(accounts: &[AccountInfo], args: &InitializeMint) -> ProgramResult {
     process_initialize_mint(accounts, args, false)
 }
--- p-token/src/processor/initialize_multisig2.rs
+++ p-token/src/processor/initialize_multisig2.rs
@@ -2,6 +2,7 @@ use pinocchio::{account_info::AccountInfo, ProgramResult};
 
 use super::initialize_multisig::process_initialize_multisig;
 
+#[inline(always)]
 pub fn process_initialize_multisig2(accounts: &[AccountInfo], m: u8) -> ProgramResult {
     process_initialize_multisig(accounts, m, false)
 }
