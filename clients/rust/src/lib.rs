b38938e3ad4abe8fa242c80e67391506e09a11af
Jon C <me@jonc.dev>
2024-12-18 18:59:21 +0100

    CI: Add program and all extra steps
    #### Problem
    
    The token program now lives in its program-specific repo, but no part of
    it is being exercised by CI.
    
    #### Summary of changes
    
    Integrate the token program properly into CI. This includes:
    
    * add it to the Cargo workspace, and removing spl-token-client, since
      it's currently empty
    * run lint / format / test on the program
    * run spellcheck / audit / semver checks on the Rust packages
    * move rust scripts to `scripts/rust` and make them reusable
    * update to Solana 2.1 and proper rust toolchains

--- clients/rust/src/lib.rs
+++ clients/rust/src/lib.rs
@@ -1,4 +1,6 @@
+/*
 mod generated;
 
 pub use generated::programs::TOKEN_ID as ID;
 pub use generated::*;
+*/
--- scripts/rust/lint.mjs
+++ scripts/rust/lint.mjs
@@ -7,18 +7,23 @@ import {
   workingDirectory,
 } from '../utils.mjs';
 
+const [folder, ...args] = cliArguments();
+
 // Configure additional arguments here, e.g.:
 // ['--arg1', '--arg2', ...cliArguments()]
-const lintArgs = cliArguments();
+const lintArgs = [
+  '-Zunstable-options',
+  '--all-targets',
+  '--all-features',
+  '--',
+  '--deny=warnings',
+  '--deny=clippy::arithmetic_side_effects',
+  ...args,
+];
 
 const fix = popArgument(lintArgs, '--fix');
 const toolchain = getToolchainArgument('lint');
-const manifestPath = path.join(
-  workingDirectory,
-  'clients',
-  'rust',
-  'Cargo.toml'
-);
+const manifestPath = path.join(workingDirectory, folder, 'Cargo.toml');
 
 // Check the client using Clippy.
 if (fix) {
