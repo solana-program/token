0dca05fbd5e68ba30c9543ccd18c796b049133c1
dkcumming <daniel.cumming@runtimeverification.com>
2025-07-29 15:33:15 +1000

    Added spec and updated output for InitializeAccount2

--- p-token/src/entrypoint.rs
+++ p-token/src/entrypoint.rs
@@ -287,18 +287,7 @@ pub(crate) fn inner_process_instruction(
             #[cfg(feature = "logging")]
             pinocchio::msg!("Testing Instruction: InitializeAccount2");
 
-            let accounts = [
-                accounts[0].clone(), // New Account Info
-                accounts[1].clone(), // Mint Info
-                // accounts[2].clone // Owner Info
-            ];
-
-            let instruction_data = [
-                // Owner Pubkey (If not provided in `accounts`)
-                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-            ];
-
-            test_process_initialize_account2(&accounts, &instruction_data)
+            test_process_initialize_account2(&accounts.first_chunk().unwrap(), &instruction_data.first_chunk().unwrap())
         }
         // 18 - InitializeAccount3
         18 => {
@@ -583,9 +572,73 @@ pub fn test_process_burn_checked(accounts: &[AccountInfo; 3], instruction_data: 
     process_burn_checked(accounts, instruction_data)
 }
 
+/// accounts[0] // New Account Info
+/// accounts[1] // Mint Info
+/// accounts[2] // Rent Sysvar Info
+/// instruction_data[..] // Owner
 #[inline(never)]
-pub fn test_process_initialize_account2(accounts: &[AccountInfo; 2], instruction_data: &[u8; 32]) -> ProgramResult {
-    process_initialize_account2(accounts, instruction_data)
+pub fn test_process_initialize_account2(accounts: &[AccountInfo; 3], instruction_data: &[u8; 32]) -> ProgramResult {
+    use spl_token_interface::state::{account, account_state};
+
+    // TODO: requires accounts[..] are all valid ptrs
+
+    //-Helpers-----------------------------------------------------------------
+    let get_account = |account_info: &AccountInfo| unsafe {
+        (account_info.borrow_data_unchecked().as_ptr() as *const account::Account)
+            .read()
+    };
+
+    //-Initial State-----------------------------------------------------------
+    let initial_state_new_account =  get_account(&accounts[0])
+        .account_state()
+        .unwrap();
+
+    let minimum_balance = unsafe {
+        pinocchio::sysvars::rent::Rent::from_bytes_unchecked(accounts[2].borrow_data_unchecked())
+    }.minimum_balance(accounts[0].data_len());
+
+    let is_native_mint = accounts[1].key() == &spl_token_interface::native_mint::ID;
+
+    let mint_is_initialised = unsafe {
+        (accounts[1].borrow_data_unchecked().as_ptr() as *const spl_token_interface::state::mint::Mint)
+            .read()
+            .is_initialized()
+    };
+
+    //-Process Instruction-----------------------------------------------------
+    let result = process_initialize_account2(accounts, instruction_data);
+
+    //-Assert Postconditions---------------------------------------------------
+    if accounts.len() < 3 {
+        assert_eq!(result, Err(ProgramError::NotEnoughAccountKeys));
+    } else if accounts[2].key() != &pinocchio::sysvars::rent::RENT_ID {
+        assert_eq!(result, Err(ProgramError::InvalidArgument))
+    } else if accounts[0].data_len() != account::Account::LEN {
+        assert_eq!(result, Err(ProgramError::InvalidAccountData))
+    } else if initial_state_new_account != account_state::AccountState::Uninitialized  { // Untested
+        assert_eq!(result, Err(ProgramError::Custom(6)))
+    } else if accounts[0].lamports() < minimum_balance {
+        assert_eq!(result, Err(ProgramError::Custom(0)))
+    } else if !is_native_mint && unsafe {accounts[1].owner()} != &spl_token_interface::program::ID {
+        assert_eq!(result, Err(ProgramError::IncorrectProgramId))
+    } else if !is_native_mint
+            && unsafe {accounts[1].owner()} == &spl_token_interface::program::ID
+            && !mint_is_initialised.unwrap() {
+        assert_eq!(result, Err(ProgramError::Custom(2)))
+    } else {
+        assert!(result.is_ok());
+        assert_eq!(get_account(&accounts[0]).account_state().unwrap(), account_state::AccountState::Initialized);
+        assert_eq!(get_account(&accounts[0]).mint, *accounts[1].key());
+        assert_eq!(get_account(&accounts[0]).owner, *instruction_data);
+
+        if is_native_mint {
+            assert!(get_account(&accounts[0]).is_native());
+            assert_eq!(get_account(&accounts[0]).native_amount().unwrap(), minimum_balance);
+            assert_eq!(get_account(&accounts[0]).amount(), accounts[0].lamports() - minimum_balance);
+        }
+    }
+
+    result
 }
 
 #[inline(never)]
--- p-token/tests/initialize_account2.rs
+++ p-token/tests/initialize_account2.rs
@@ -85,3 +85,66 @@ async fn initialize_account2() {
     assert!(account.owner == owner);
     assert!(account.mint == mint);
 }
+
+#[tokio::test]
+async fn initialize_account2_native_mint() {
+    let context = ProgramTest::new("pinocchio_token_program", TOKEN_PROGRAM_ID, None)
+        .start_with_context()
+        .await;
+
+    let mint = Pubkey::from(spl_token_interface::native_mint::ID);
+
+    // Given a mint authority, freeze authority and an account keypair.
+
+    let owner = Pubkey::new_unique();
+    let account = Keypair::new();
+
+    let account_size = 165;
+    let rent = context.banks_client.get_rent().await.unwrap();
+
+    let initialize_ix = spl_token::instruction::initialize_account2(
+        &spl_token::ID,
+        &account.pubkey(),
+        &mint,
+        &owner,
+    )
+    .unwrap();
+
+    // When a new mint account is created and initialized.
+
+    let instructions = vec![
+        create_account(
+            &context.payer.pubkey(),
+            &account.pubkey(),
+            rent.minimum_balance(account_size),
+            account_size as u64,
+            &TOKEN_PROGRAM_ID,
+        ),
+        initialize_ix,
+    ];
+
+    let tx = Transaction::new_signed_with_payer(
+        &instructions,
+        Some(&context.payer.pubkey()),
+        &[&context.payer, &account],
+        context.last_blockhash,
+    );
+    context.banks_client.process_transaction(tx).await.unwrap();
+
+    // Then an account has the correct data.
+
+    let account = context
+        .banks_client
+        .get_account(account.pubkey())
+        .await
+        .unwrap();
+
+    assert!(account.is_some());
+
+    let account = account.unwrap();
+    let account = spl_token::state::Account::unpack(&account.data).unwrap();
+
+    assert!(!account.is_frozen());
+    assert!(account.owner == owner);
+    assert!(account.mint == mint);
+}
