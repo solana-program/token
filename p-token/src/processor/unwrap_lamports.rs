10ff7e517c8007ec8be8986523db39710a9a89c8
febo <febo@anza.xyz>
2025-09-02 14:22:38 +0100

    Add processor

---
+++ p-token/src/processor/unwrap_lamports.rs
@@ -0,0 +1,66 @@
+use {
+    super::validate_owner,
+    crate::processor::{check_account_owner, unpack_amount},
+    pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult},
+    pinocchio_token_interface::{
+        error::TokenError,
+        state::{account::Account, load_mut},
+    },
+};
+
+#[allow(clippy::arithmetic_side_effects)]
+pub fn process_unwrap_lamports(accounts: &[AccountInfo], instruction_data: &[u8]) -> ProgramResult {
+    // Amount being unwrapped.
+    let amount = unpack_amount(instruction_data)?;
+
+    let [source_account_info, destination_account_info, authority_info, remaining @ ..] = accounts
+    else {
+        return Err(ProgramError::NotEnoughAccountKeys);
+    };
+
+    // SAFETY: single immutable borrow to `source_account_info` account data
+    let source_account =
+        unsafe { load_mut::<Account>(source_account_info.borrow_mut_data_unchecked())? };
+
+    if !source_account.is_native() {
+        return Err(TokenError::NonNativeNotSupported.into());
+    }
+
+    // SAFETY: `authority_info` is not currently borrowed; in the case `authority_info` is
+    // the same as `source_account_info`, then it cannot be a multisig.
+    unsafe { validate_owner(&source_account.owner, authority_info, remaining)? };
+
+    let remaining_amount = source_account
+        .amount()
+        .checked_sub(amount)
+        .ok_or(TokenError::InsufficientFunds)?;
+
+    // Comparing whether the AccountInfo's "point" to the same account or
+    // not - this is a faster comparison since it just checks the internal
+    // raw pointer.
+    let self_transfer = source_account_info == destination_account_info;
+
+    if self_transfer || amount == 0 {
+        // Validates the token account owner since we are not writing
+        // to the account.
+        check_account_owner(source_account_info)?;
+    } else {
+        source_account.set_amount(remaining_amount);
+
+        // SAFETY: single mutable borrow to `source_account_info` lamports.
+        let source_lamports = unsafe { source_account_info.borrow_mut_lamports_unchecked() };
+        // Note: The amount of a source token account is already validated and the
+        // `lamports` on the account is always greater than `amount`.
+        *source_lamports -= amount;
+
+        // SAFETY: single mutable borrow to `destination_account_info` lamports; the
+        // account is already validated to be different from
+        // `source_account_info`.
+        let destination_lamports =
+            unsafe { destination_account_info.borrow_mut_lamports_unchecked() };
+        // Note: The total lamports supply is bound to `u64::MAX`.
+        *destination_lamports += amount;
+    }
+
+    Ok(())
+}
