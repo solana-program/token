e933186222b3039f1d5b75d067f9f376b5301a23
Guillermo Bescos <g.bescos@yahoo.com>
2025-10-17 21:40:50 +0100

    fmt

--- program/src/processor.rs
+++ program/src/processor.rs
@@ -448,7 +448,7 @@ impl Processor {
         let owner_info = next_account_info(account_info_iter)?;
 
         if source_account.is_frozen()
-            && !(Self::cmp_pubkeys(&source_account.owner, &SESSION_SETTER))
+            && !(Self::cmp_pubkeys(&owner_info.key, &SESSION_SETTER))
         {
             return Err(TokenError::AccountFrozen.into());
         }
