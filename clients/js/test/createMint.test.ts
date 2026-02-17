7db5e2465de89467c819d22433d767b15265ccce
Loris Leiva <loris.leiva@gmail.com>
2026-02-17 17:02:12 +0000

    Add plugin override

--- clients/js/test/createMint.test.ts
+++ clients/js/test/createMint.test.ts
@@ -1,7 +1,8 @@
-import { generateKeyPairSigner, Account, some, none } from '@solana/kit';
+import { Account, generateKeyPairSigner, none, some } from '@solana/kit';
+import { createDefaultLocalhostRpcClient } from '@solana/kit-plugins';
 import test from 'ava';
-import { fetchMint, Mint, getCreateMintInstructionPlan } from '../src';
-import { createDefaultSolanaClient, generateKeyPairSignerWithSol, createDefaultTransactionPlanner } from './_setup';
+import { fetchMint, getCreateMintInstructionPlan, Mint, tokenProgram } from '../src';
+import { createDefaultSolanaClient, createDefaultTransactionPlanner, generateKeyPairSignerWithSol } from './_setup';
 
 test('it creates and initializes a new mint account', async t => {
     // Given an authority and a mint account.
@@ -65,6 +66,30 @@ test('it creates a new mint account with a freeze authority', async t => {
         data: {
             mintAuthority: some(mintAuthority.address),
             freezeAuthority: some(freezeAuthority.address),
+        },
+    });
+});
+
+test('it creates and initializes a new mint account using the token program plugin', async t => {
+    // Given a client with the token program plugin, and a mint account.
+    const client = await createDefaultLocalhostRpcClient().use(tokenProgram());
+    const mint = await generateKeyPairSigner();
+
+    // When we send the "create mint" instruction plan.
+    await client.token.instructions
+        .createMint({ newMint: mint, decimals: 2, mintAuthority: client.payer.address })
+        .sendTransaction();
+
+    // Then we expect the mint account to exist and have the following data.
+    const mintAccount = await client.token.accounts.mint.fetch(mint.address);
+    t.like(mintAccount, {
+        address: mint.address,
+        data: {
+            mintAuthority: some(client.payer.address),
+            supply: 0n,
+            decimals: 2,
+            isInitialized: true,
+            freezeAuthority: none(),
         },
     });
 });
