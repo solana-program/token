61df6555ab3651f32bccfc260cd6adf34afdb69c
Loris Leiva <loris.leiva@gmail.com>
2026-02-17 17:12:25 +0000

    Extract custom plugin in separate file

--- clients/js/src/index.ts
+++ clients/js/src/index.ts
@@ -1,45 +1,6 @@
 export * from './generated';
+export { tokenProgram, type TokenPlugin, type TokenPluginInstructions, type TokenPluginRequirements } from './plugin';
+
 export * from './createMint';
 export * from './mintToATA';
 export * from './transferToATA';
-
-import { ClientWithPayer, pipe } from '@solana/kit';
-import {
-    tokenProgram as generatedTokenProgram,
-    TokenPlugin as GeneratedTokenPlugin,
-    TokenPluginInstructions as GeneratedTokenPluginInstructions,
-    TokenPluginRequirements as GeneratedTokenPluginRequirements,
-} from './generated';
-import { CreateMintInstructionPlanInput, getCreateMintInstructionPlan } from './createMint';
-import { addSelfPlanAndSendFunctions, SelfPlanAndSendFunctions } from '@solana/kit/program-client-core';
-
-export type TokenPluginRequirements = GeneratedTokenPluginRequirements & ClientWithPayer;
-
-export type TokenPlugin = Omit<GeneratedTokenPlugin, 'instructions'> & { instructions: TokenPluginInstructions };
-
-export type TokenPluginInstructions = GeneratedTokenPluginInstructions & {
-    createMint: (
-        input: MakeOptional<CreateMintInstructionPlanInput, 'payer'>,
-    ) => ReturnType<typeof getCreateMintInstructionPlan> & SelfPlanAndSendFunctions;
-};
-
-export function tokenProgram() {
-    return <T extends TokenPluginRequirements>(client: T) => {
-        return pipe(client, generatedTokenProgram(), c => ({
-            ...c,
-            token: <TokenPlugin>{
-                ...c.token,
-                instructions: {
-                    ...c.token.instructions,
-                    createMint: input =>
-                        addSelfPlanAndSendFunctions(
-                            client,
-                            getCreateMintInstructionPlan({ ...input, payer: input.payer ?? client.payer }),
-                        ),
-                },
-            },
-        }));
-    };
-}
-
-type MakeOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
