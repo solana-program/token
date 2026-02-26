35d944483a38f81b0ee43d7546fd5314d45aad57
Jon C <me@jonc.dev>
2026-02-26 20:10:42 +0100

    p-token: Sync native updates rent exempt reserve
    #### Problem
    
    This is the p-token version of
    https://github.com/solana-program/token-2022/pull/1006.
    
    The current rent-exempt reserve at the time of account creation is
    stored in the `is_native` field of a token account. When rent is
    decreased, or ever increased, this value will be incorrect, leading to
    an incorrect `amount` stored in the token account.
    
    #### Summary of changes
    
    Update `sync_native` to use the current rent-exempt reserve, and allow
    for the amount to potentially decrease.

--- pinocchio/program/src/processor/sync_native.rs
+++ pinocchio/program/src/processor/sync_native.rs
@@ -1,6 +1,11 @@
 use {
     super::check_account_owner,
-    pinocchio::{account_info::AccountInfo, program_error::ProgramError, ProgramResult},
+    pinocchio::{
+        account_info::AccountInfo,
+        program_error::ProgramError,
+        sysvars::{rent::Rent, Sysvar},
+        ProgramResult,
+    },
     pinocchio_token_interface::{
         error::TokenError,
         state::{account::Account, load_mut},
@@ -13,20 +18,20 @@ pub fn process_sync_native(accounts: &[AccountInfo]) -> ProgramResult {
 
     check_account_owner(native_account_info)?;
 
+    let rent = Rent::get()?;
+    let rent_exempt_reserve = rent.minimum_balance(native_account_info.data_len());
+
     // SAFETY: single mutable borrow to `native_account_info` account data and
     // `load_mut` validates that the account is initialized.
     let native_account =
         unsafe { load_mut::<Account>(native_account_info.borrow_mut_data_unchecked())? };
 
-    if let Option::Some(rent_exempt_reserve) = native_account.native_amount() {
+    if native_account.is_native() {
         let new_amount = native_account_info
             .lamports()
             .checked_sub(rent_exempt_reserve)
             .ok_or(TokenError::Overflow)?;
-
-        if new_amount < native_account.amount() {
-            return Err(TokenError::InvalidState.into());
-        }
+        native_account.set_native_amount(rent_exempt_reserve);
         native_account.set_amount(new_amount);
     } else {
         return Err(TokenError::NonNativeNotSupported.into());
