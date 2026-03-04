b42c3f127ec787c16ae8533a9d7b2ec80d3b8b9f
febo <febo@anza.xyz>
2026-03-04 12:21:44 +0000

    Improve remaining check

--- pinocchio/program/src/processor/sync_native.rs
+++ pinocchio/program/src/processor/sync_native.rs
@@ -20,15 +20,13 @@ pub fn process_sync_native(accounts: &[AccountInfo]) -> ProgramResult {
 
     check_account_owner(native_account_info)?;
 
-    let rent_exempt_reserve = if remaining.is_empty() {
-        Rent::get()?.minimum_balance(native_account_info.data_len())
-    } else {
-        // SAFETY: `remaining` is guaranteed to not be empty.
-        let rent_sysvar_info = unsafe { remaining.get_unchecked(0) };
+    let rent_exempt_reserve = if let Some(rent_sysvar_info) = remaining.first() {
         // SAFETY: single immutable borrow to `rent_sysvar_info`; account ID and length
         // are checked by `from_account_info_unchecked`.
         let rent = unsafe { Rent::from_account_info_unchecked(rent_sysvar_info)? };
         rent.minimum_balance(native_account_info.data_len())
+    } else {
+        Rent::get()?.minimum_balance(native_account_info.data_len())
     };
 
     // SAFETY: single mutable borrow to `native_account_info` account data and
